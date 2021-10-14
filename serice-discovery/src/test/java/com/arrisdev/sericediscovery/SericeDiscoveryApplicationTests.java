784ffc1485e175b5af91acd9f8874324418d8a55
arris manduma <85527404+arrMan-dev@users.noreply.github.com>
2021-10-14 16:40:52 -0500

    initial commit

---
+++ serice-discovery/src/test/java/com/arrisdev/sericediscovery/SericeDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrisdev.sericediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SericeDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
